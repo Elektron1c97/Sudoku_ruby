dba253982aff972479738a866bacc05562f07e33
Elektron1c97 <Elektron1c97@gmail.com>
2015-09-18 10:59:49 +0200

    added human solver

---
+++ lib/Sudoku_ruby/solver/human_solver.rb
@@ -0,0 +1,154 @@
+module SudokuRuby
+
+  module HumanSolver
+
+    class SudokuTooHardError < StandardError; end
+    class SudokuUnsolvable < StandardError; end
+
+    class PartialSolution
+
+      def initialize(arr_to_solve, position = 0)
+        @position = position
+        @arr = arr_to_solve
+        @empty_fields = empty_field_indexes
+        @start_point = find_start_index
+      end
+
+      def next
+        if find_start_index.values.first.length == 1
+          a = @arr.map.with_index do |e, i|
+            if @empty_fields.include?(i)
+              free_nums = free_nums_for_index_and_input(i, @arr)
+              free_nums.first if free_nums.length == 1
+            else
+              e
+            end
+          end
+          return self.class.new a
+        elsif find_start_index.values.first.length == 0
+          return :unsolvable
+        else
+          return :too_hard
+          # raise SudokuTooHardError.new "This sudoku is too hard for me, use AdvancedPartialSolution"
+        end
+      end
+
+      def find_start_index
+        @empty_fields.map { |e| { e => free_nums_for_index_and_input(e, @arr) } }.sort { |x, y| x.values.first.length <=> y.values.first.length}.first
+      end
+
+      def free_nums_for_index_and_input(index, input)
+        b = Board.new(input, 9)
+        (1..9).to_a - [b.row(row_for_index(index)), b.column(col_for_index(index)), b.field_group(field_group_for_index(index))].flatten.compact.uniq
+      end
+
+      def free_nums_for_current_field
+        free_nums_for_index_and_input(@empty_fields[@position], @arr)
+      end
+
+      def empty_field_indexes
+        @arr.map.with_index { |e, i| [i, e] }.select { |e| e[1].nil? }.map { |e| e[0] }
+      end
+
+      def current_row
+        row_for_index(@position)
+      end
+
+      def current_col
+        col_for_index(@position)
+      end
+
+      def current_field_group
+        field_group_for_index(@position)
+      end
+
+      def row_for_index(index)
+        (index / 9).to_i
+      end
+
+      def col_for_index(index)
+        index % 9
+      end
+
+      def field_group_for_index(index)
+        r = (row_for_index(index) / 3).to_i * 3
+        c = (col_for_index(index) / 3).to_i
+        r + c
+      end
+
+      # Function to detect if the current step is the solution
+      def solution?
+        board = Board.new(@arr)
+        @arr.all? && board.valid?
+      end
+
+      def count(obj)
+        @arr.count(obj)
+      end
+
+      def to_a
+        @arr
+      end
+
+      def ==(other)
+        to_a == other.to_a
+      end
+
+    end
+
+    class AdvancedPartialSolution < PartialSolution
+      def next
+        case result = super
+        when :too_hard
+          a = find_start_index.flat_map do |index, available|
+            available.map do |a|
+              arr_cpy = @arr.dup
+              arr_cpy[index] = a
+              self.class.new arr_cpy
+            end
+          end
+          return PartialSolutionCollection.new a
+        else
+          result
+        end
+      end
+    end
+
+    class PartialSolutionCollection
+      def initialize(solutions)
+        if solutions.size < 2
+          raise ArgumentError, "Can not create collection without or with a single solution: #{solutions.inspect}"
+        end
+        @solutions = solutions
+      end
+
+      def next
+        other_guesses = @solutions[1..-1]
+        begin
+          self.class.new [@solutions.first.next] + other_guesses
+        rescue SudokuUnsolvable
+          if other_guesses.size == 1
+            other_guesses.first
+          else
+            self.class.new other_guesses
+          end
+        end
+      end
+
+      def solution?
+        @solutions.first.solution?
+      end
+
+      def to_a
+        @solutions.first.to_a
+      end
+
+      def ==(other)
+        if other.is_a?(self.class)
+          @solutions == other.instance_variable_get("@solutions")
+        end
+      end
+    end
+
+  end
+end
---
+++ test/unit/human_solver_test.rb
@@ -0,0 +1,212 @@
+require_relative '../test_helper'
+
+class HumanSolverTest < Minitest::Test
+  include SudokuRuby::HumanSolver
+
+  RETARD_INPUT = [1, 2, 4, 3, 5, 9, 6, 7, 8,
+                  3, 6, 5, 1, 7, 8, 2, nil, 9,
+                  7, 9, 8, 4, 6, 2, 1, 3, 5,
+                  2, 5, 3, 6, 4, 7, 8, 9, 1,
+                  4, 7, 9, 8, 1, 5, 3, 2, 6,
+                  nil, 8, 1, 2, 9, 3, 7, 5, 4,
+                  5, 4, 2, 7, 8, 6, 9, 1, 3,
+                  8, 1, 7, 9, 3, 4, 5, 6, 2,
+                  9, 3, 6, 5, 2, 1, 4, 8, 7]
+
+  EASY_INPUT = [1, 2, nil, 4, nil, 6, 7, 8, 9,
+                nil, 5, 9, 3, 7, 8, 1, 6, nil,
+                6, 7, 8, nil, 1, 2, nil, 5, 3,
+                2, 1, nil, 6, 3, nil, 8, 9, 4,
+                3, 8, 4, 2, 9, 1, 5, nil, 6,
+                7, 9, 6, 5, 8, 4, nil, 2, 1,
+                nil, 3, 2, nil, nil, 9, 6, 1, nil,
+                9, 4, 7, nil, nil, 5, 2, nil, 8,
+                8, 6, nil, 7, nil, nil, 9, nil, 5]
+
+  HARD_INPUT = [nil, nil, nil, 4, 5, 7, nil, nil, nil,
+                nil, 5, nil, nil, nil, 9, 1, nil, nil,
+                6, 8, 9, 2, nil, nil, 4, 5, nil,
+                3, 4, 1, 7, nil, 6, nil, nil, nil,
+                5, nil, nil, 9, nil, 1, 3, 7, nil,
+                7, 9, 2, nil, nil, nil, nil, 1, 6,
+                2, nil, nil, 1, nil, 4, 7, 6, 8,
+                nil, 7, nil, nil, nil, 2, nil, nil, 1,
+                9, 1, nil, nil, nil, nil, 5, 3, nil]
+
+  VERY_HARD_INPUT = [nil, 1, nil, nil, 2, 6, nil, nil, 3,
+                     nil, nil, nil, nil, 5, nil, nil, 2, nil,
+                     3, nil, nil, nil, nil, 4, 7, nil, nil,
+                     nil, 6, 7, nil, 3, nil, nil, nil, 4,
+                     nil, nil, 4, 6, nil, 2, 3, nil, nil,
+                     8, nil, nil, nil, 7, nil, 6, 5, nil,
+                     nil, nil, 8, 5, nil, nil, nil, nil, 2,
+                     nil, 7, nil, nil, 4, nil, nil, nil, nil,
+                     6, nil, nil, 2, 1, nil, nil, 9, nil]
+
+   UNSOLVABLE_INPUT = [ 5, 1, 6, 8, 4, 9, 7, 3, 2,
+                        3, nil, 7, 6, nil, 5, nil, nil, nil,
+                        8, nil, 9, 7, nil, nil, nil, 6, 5,
+                        1, 3, 5, nil, 6, nil, 9, nil, 7,
+                        4, 7, 2, 5, 9, 1, nil, nil, 6,
+                        9, 6, 8, 3, 7, nil, nil, 5, nil,
+                        2, 5, 3, 1, 8, 6, nil, 7, 4,
+                        6, 8, 4, 2, nil, 7, 5, nil, nil,
+                        7, 9, 1, nil, 5, nil, 6, nil, 8]
+
+  def test_first_row
+    sol = PartialSolution.new(RETARD_INPUT, 5)
+    assert_equal 0, sol.current_row
+  end
+
+  def test_col_of_field_in_first_row
+    sol = PartialSolution.new(RETARD_INPUT, 4)
+    assert_equal 4, sol.current_col
+  end
+
+  def test_row_of_random_field
+    sol = PartialSolution.new(RETARD_INPUT, 39)
+    assert_equal 4, sol.current_row
+  end
+
+  def test_col_of_random_field
+    sol = PartialSolution.new(RETARD_INPUT, 43)
+    assert_equal 7, sol.current_col
+  end
+
+  def test_field_group_in_first_field_group
+    sol = PartialSolution.new(RETARD_INPUT, 4)
+    assert_equal 1, sol.current_field_group
+  end
+
+  def test_field_group_in_fourth_field_group
+    sol = PartialSolution.new(RETARD_INPUT, 39)
+    assert_equal 4, sol.current_field_group
+  end
+
+  def test_empty_fields_for_input_indexes
+    sol = PartialSolution.new(RETARD_INPUT, 39)
+    assert_equal [16, 45], sol.empty_field_indexes
+  end
+
+  def test_free_nums_for_first_empty_field
+    sol = PartialSolution.new(RETARD_INPUT)
+    assert_equal [4], sol.free_nums_for_current_field
+  end
+
+  def test_insert_all_with_only_one_possibility
+    expected_filled = [1, nil, 3, 4, 5, 7, nil, nil, nil,
+                       4, 5, nil, nil, nil, 9, 1, nil, nil,
+                       6, 8, 9, 2, nil, 3, 4, 5, nil,
+                       3, 4, 1, 7, nil, 6, nil, nil, nil,
+                       5, 6, nil, 9, nil, 1, 3, 7, nil,
+                       7, 9, 2, nil, nil, nil, 8, 1, 6,
+                       2, 3, nil, 1, nil, 4, 7, 6, 8,
+                       nil, 7, nil, nil, nil, 2, 9, nil, 1,
+                       9, 1, nil, nil, nil, 8, 5, 3, nil]
+    sol = PartialSolution.new(HARD_INPUT)
+    assert_equal expected_filled, sol.next.to_a
+  end
+
+  def test_ended_solution
+    retard_input_solution = [1, 2, 4, 3, 5, 9, 6, 7, 8,
+                             3, 6, 5, 1, 7, 8, 2, 4, 9,
+                             7, 9, 8, 4, 6, 2, 1, 3, 5,
+                             2, 5, 3, 6, 4, 7, 8, 9, 1,
+                             4, 7, 9, 8, 1, 5, 3, 2, 6,
+                             6, 8, 1, 2, 9, 3, 7, 5, 4,
+                             5, 4, 2, 7, 8, 6, 9, 1, 3,
+                             8, 1, 7, 9, 3, 4, 5, 6, 2,
+                             9, 3, 6, 5, 2, 1, 4, 8, 7]
+    sol = PartialSolution.new(RETARD_INPUT)
+    sol = sol.next until sol.solution?
+    assert_equal retard_input_solution, sol.to_a
+  end
+
+  def test_solving_empty_board
+    sol = AdvancedPartialSolution.new([nil] * 81)
+    until sol.solution?
+      sol = sol.next
+    end
+    board = SudokuRuby::Board.new(sol.to_a)
+    assert board.valid?
+  end
+
+  def test_collection_if_only_two_or_more_possibilites
+    sol = AdvancedPartialSolution.new(VERY_HARD_INPUT)
+    assert sol.next.is_a?(PartialSolutionCollection), "Expected PartialSolutionCollection, got: #{sol.class}"
+  end
+
+  def test_unsolvable_solution_raises_exception
+    data = [1,2,3,4,5,6,7,8,9] * 9
+    data.pop
+    sol = AdvancedPartialSolution.new(data + [nil])
+    assert_equal :unsolvable, sol.next
+  end
+
+  def test_whole_solution_for_very_hard_input
+    sol = AdvancedPartialSolution.new(VERY_HARD_INPUT)
+    until sol.solution?
+      sol = sol.next
+    end
+  end
+end
+
+class PartialSolutionCollectionTest < Minitest::Test
+  include SudokuRuby::HumanSolver
+
+  def test_cant_create_without_any_solutions
+    assert_raises ArgumentError do
+      PartialSolutionCollection.new([])
+    end
+  end
+
+  def test_cant_create_with_a_single_solution
+    assert_raises ArgumentError do
+      PartialSolutionCollection.new([Object.new])
+    end
+  end
+
+  class DummyPartialSolution
+    attr_reader :next_called
+    def initialize(next_solution = nil)
+      @next_solution = next_solution
+      @next_called = 0
+    end
+
+    def next
+      @next_called += 1
+      @next_solution
+    end
+  end
+
+  class UnsolvablePartialSolution < DummyPartialSolution
+    def next
+      super
+      raise SudokuRuby::HumanSolver::SudokuUnsolvable
+    end
+  end
+
+  def test_keep_following_current_guess_while_possible
+    solutions = [current_guess = DummyPartialSolution.new(followup_guess = DummyPartialSolution.new), other_guess = DummyPartialSolution.new]
+    collection = PartialSolutionCollection.new(solutions)
+    next_collection = collection.next
+    assert_equal PartialSolutionCollection.new([followup_guess, other_guess]), next_collection
+    assert_equal 1, current_guess.next_called
+    assert_equal 0, other_guess.next_called
+  end
+
+  def test_resume_with_next_guess_when_first_didnt_work_out
+    solutions = [current_guess = UnsolvablePartialSolution.new, other_guess = DummyPartialSolution.new, yet_another_guess = DummyPartialSolution.new]
+    collection = PartialSolutionCollection.new(solutions)
+    next_collection = collection.next
+    assert_equal PartialSolutionCollection.new([other_guess, yet_another_guess]), next_collection
+    assert_equal 1, current_guess.next_called
+    assert_equal 0, other_guess.next_called
+  end
+
+  def test_unwrap_collection_when_only_a_single_solution_is_left
+    solutions = [current_guess = UnsolvablePartialSolution.new, other_guess = DummyPartialSolution.new]
+    collection = PartialSolutionCollection.new(solutions)
+    assert_equal other_guess, collection.next
+  end
+end
