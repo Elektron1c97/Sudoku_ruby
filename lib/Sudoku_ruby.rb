b45b6991725c1f0b3d33f47c91449cee40cd5fa2
Elektron1c97 <Elektron1c97@gmail.com>
2015-09-21 09:33:30 +0200

    added human solver

--- lib/Sudoku_ruby.rb
+++ lib/Sudoku_ruby.rb
@@ -1,5 +1,6 @@
 $:.unshift File.dirname(__FILE__)
 require "Sudoku_ruby/solver/backtrack_brute_force_solver"
+require "Sudoku_ruby/solver/human_solver"
 require "Sudoku_ruby/generator"
 require "Sudoku_ruby/board"
 require "Sudoku_ruby/version"
